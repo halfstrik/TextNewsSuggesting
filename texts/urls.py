d8ea929721c5b5bea3accca01f7d6492fbbcbf8e
Strikki <halfstrik@gmail.com>
2015-06-04 14:40:56 +0300

    Add net of general tags only. When click on the tag in the net - this tag and all of it's neighbors will be highlighted.

--- texts/urls.py
+++ texts/urls.py
@@ -4,4 +4,6 @@ from texts import views
 
 urlpatterns = [
     url(r'^tags_relationships_json$', views.tags_relationships_json, name='tags_relationships_json'),
+    url(r'^general_tags_relationships_json$', views.general_tags_relationships_json,
+        name='general_tags_relationships_json$'),
 ]
--- texts/views.py
+++ texts/views.py
@@ -23,3 +23,25 @@ def tags_relationships_json(request):
                       'type': 'curve'})
 
     return HttpResponse(json.dumps({'edges': edges, 'nodes': nodes}))
+
+
+def general_tags_relationships_json(request):
+    tags = Tag.objects.usage_for_model(Text, counts=True)
+    nodes = []
+    tag_ids = []
+    for name, count, tag_id in [(tag.name, tag.count, tag.id) for tag in tags]:
+        if ':' in name:
+            continue
+        nodes.append({'label': name, 'size': str(count), 'id': str(tag_id)})
+        tag_ids.append(tag_id)
+    relationships = TagRelationship.objects.filter(first_tag__in=tag_ids).filter(second_tag__in=tag_ids)
+    edges = []
+    for relationship in relationships:
+        edges.append({'source': str(relationship.first_tag.id),
+                      'target': str(relationship.second_tag.id),
+                      'id': str(relationship.id),
+                      'label': str(relationship.weigh),
+                      'size': str(2 * relationship.weigh),
+                      'type': 'curve'})
+
+    return HttpResponse(json.dumps({'edges': edges, 'nodes': nodes}))
