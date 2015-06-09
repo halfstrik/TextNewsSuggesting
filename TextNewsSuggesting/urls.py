9bf2293464b79dd12b50e9cbb10c161098620823
Strikki <halfstrik@gmail.com>
2015-06-09 11:30:04 +0300

    Added redirect to admin

--- TextNewsSuggesting/urls.py
+++ TextNewsSuggesting/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/admin')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^texts/', include('texts.urls')),
 ]
